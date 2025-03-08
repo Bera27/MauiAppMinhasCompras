a05a311cc7b89423395c42e4e305d8a486994c29
Gabriel Bera <g.bera2710@gmail.com>
2025-03-08 15:08:57 -0300

    Incompleto

--- MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
+++ MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -1,12 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MauiAppMinhasCompras.Models;
+using SQLite;
 
 namespace MauiAppMinhasCompras.Helpers
 {
-    class SQLiteDatabaseHelper
+    public class SQLiteDatabaseHelper
     {
+        readonly SQLiteAsyncConnection _connection;
+
+        public SQLiteDatabaseHelper(string path)
+        {
+            _connection = new SQLiteAsyncConnection(path);
+            _connection.CreateTableAsync<Produto>().Wait();
+        }
+
+        public Task<int> Insert(Produto produto) 
+        {
+            return _connection.InsertAsync(produto);
+        }
+
+        public Task<List<Produto>> Update(Produto produto) 
+        {
+            string sql = "UPDATE Produto SET Descricao=?, Quantidade=?, Preco=? WHERE Id=?";
+
+            return _connection.QueryAsync<Produto>(sql , produto.Descricao, produto.Quantidade, produto.Preco, produto.Id);
+        }
+
+        public Task<int> Delete(int id) 
+        {
+            return _connection.Table<Produto>().DeleteAsync(i => i.Id == id);
+        }
+
+        public Task<List<Produto>> GetAll() 
+        {
+            return _connection.Table<Produto>().ToListAsync();
+        }
+
+        public Task<List<Produto>> Search(string query) 
+        {
+            string sql = "SELECT * Produto WHERE descricao LIKE '%" + query + "%'";
+
+            return _connection.QueryAsync<Produto>(sql);
+        }
     }
 }
